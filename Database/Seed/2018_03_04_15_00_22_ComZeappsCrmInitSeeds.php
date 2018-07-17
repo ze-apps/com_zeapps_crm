56de798c8d3a0a0e839fce24800a4d4dac0477ab
Nicolas Ramel <nramel@r-numerique.fr>
2018-07-17 19:15:44 +0200

    préparation crm

---
+++ Database/Seed/2018_03_04_15_00_22_ComZeappsCrmInitSeeds.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use App\com_zeapps_contact\Models\Companies;
+use App\com_zeapps_contact\Models\Contacts;
+use App\com_zeapps_contact\Models\CodeNaf;
+use App\com_zeapps_contact\Models\AccountFamilies;
+use App\com_zeapps_contact\Models\Modalities;
+use App\com_zeapps_contact\Models\ModalitiesLang;
+
+
+class ComZeappsCrmInitSeeds
+{
+    public function run()
+    {
+
+    }
+}
