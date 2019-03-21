951483942cc44de70c56e6e7f8512aa6d90549ac
Nicolas Ramel <nramel@r-numerique.fr>
2019-03-21 11:57:39 +0100

    update model Product & ProductCategories

--- Models/Product/ProductCategories.php
+++ Models/Product/ProductCategories.php
@@ -14,9 +14,9 @@ class ProductCategories extends Model
     use SoftDeletes;
 
     static protected $_table = 'com_zeapps_crm_product_categories';
-    protected $table ;
+    protected $table;
 
-    protected $fieldModelInfo ;
+    protected $fieldModelInfo;
 
 
     public function __construct(array $attributes = [])
@@ -37,24 +37,27 @@ class ProductCategories extends Model
         parent::__construct($attributes);
     }
 
-    public static function getSchema() {
-        return $schema = Capsule::schema()->getColumnListing(self::$_table) ;
+    public static function getSchema()
+    {
+        return $schema = Capsule::schema()->getColumnListing(self::$_table);
     }
 
-    public function save(array $options = []) {
+    public function save(array $options = [])
+    {
 
         /******** clean data **********/
-        $this->fieldModelInfo->cleanData($this) ;
+        $this->fieldModelInfo->cleanData($this);
 
 
         /**** to delete unwanted field ****/
-        $this->fieldModelInfo->removeFieldUnwanted($this) ;
+        $this->fieldModelInfo->removeFieldUnwanted($this);
 
         return parent::save($options);
     }
 
 
-    public static function getRootCategory(){
+    public static function getRootCategory()
+    {
         $root = array();
         $root["name"] = 'racine';
         $root["id"] = '0';
@@ -64,7 +67,8 @@ class ProductCategories extends Model
         return $root;
     }
 
-    public static function getArchiveCategory(){
+    public static function getArchiveCategory()
+    {
         $archive = array();
         $archive["name"] = 'archive';
         $archive["id"] = '-1';
@@ -113,39 +117,121 @@ class ProductCategories extends Model
         return false;
     }
 
-    public static function removeProductIn($id = array(), $parent = false, $qty = 1){
-        if($id) {
+    public static function removeProductIn($id = array(), $parent = false, $qty = 1)
+    {
+        if ($id) {
             $category = ProductCategories::where("id", $id)->first();
-            if(!$parent) {
-                $category->nb_products = $category->nb_products - $qty ;
-                $category->save() ;
-            } else{
-                $category->nb_products_r = $category->nb_products_r - $qty ;
-                $category->save() ;
+            if (!$parent) {
+                $category->nb_products = $category->nb_products - $qty;
+                $category->save();
+            } else {
+                $category->nb_products_r = $category->nb_products_r - $qty;
+                $category->save();
             }
 
-            if($category->id_parent > 0){
+            if ($category->id_parent > 0) {
                 self::removeProductIn($category->id_parent, true, $qty);
             }
         }
         return;
     }
 
-    public static function newProductIn($id = array(), $parent = false){
-        if($id) {
+    public static function newProductIn($id = array(), $parent = false)
+    {
+        if ($id) {
             $category = ProductCategories::where("id", $id)->first();
-            if(!$parent) {
-                $category->nb_products = $category->nb_products + 1 ;
+            if (!$parent) {
+                $category->nb_products = $category->nb_products + 1;
                 $category->save();
             } else {
-                $category->nb_products_r = $category->nb_products_r + 1 ;
+                $category->nb_products_r = $category->nb_products_r + 1;
                 $category->save();
             }
 
-            if($category->id_parent > 0){
+            if ($category->id_parent > 0) {
                 self::newProductIn($category->id_parent, true);
             }
         }
         return;
+    }
+
+
+    public static function turnover($year = null, $where = array())
+    {
+        $query = "SELECT SUM(l.total_ht) as total_ht,
+                         YEAR(i.date_limit) as year
+                  FROM com_zeapps_crm_product_categories ca
+                  LEFT JOIN com_zeapps_crm_products p ON p.id_cat = ca.id
+                  LEFT JOIN com_zeapps_crm_invoice_lines l ON l.id_product = p.id
+                  LEFT JOIN com_zeapps_crm_invoices i ON i.id = l.id_invoice
+                  WHERE i.finalized = '1'
+                        AND l.type = 'product'
+                        AND i.deleted_at IS NULL
+                        AND l.deleted_at IS NULL
+                        AND YEAR(i.date_limit) in (" . ($year - 1) . "," . $year . ")";
+
+        if (isset($where['id_cat'])) {
+            $query .= " AND ca.id IN (" . implode(',', $where['id_cat']) . ")";
+        }
+
+
+
+
+
+        if (isset($where['id_price_list'])) {
+            $query .= " AND i.id_price_list = " . $where['id_price_list'];
+        }
+
+        if (isset($where['id_origin'])) {
+            $query .= " AND i.id_origin = " . $where['id_origin'];
+        }
+
+        if (isset($where['delivery_country_id IN'])) {
+            $query .= " AND i.delivery_country_id IN (" . $where['delivery_country_id IN'] . ")";
+        }
+
+        if (isset($where['delivery_country_id'])) {
+            $query .= " AND i.delivery_country_id = " . $where['delivery_country_id'] ;
+        }
+
+
+
+
+
+
+        $query .= " GROUP BY YEAR(i.date_limit)";
+
+        return Capsule::select(Capsule::raw($query));
+    }
+
+    public static function turnover_details($year = null, $where = array())
+    {
+        $query = "SELECT SUM(l.total_ht) as total_ht,
+                         YEAR(i.date_limit) as year,
+                         i.id_origin as id_origin,
+                         ca.id as id_cat
+                  FROM com_zeapps_crm_product_categories ca
+                  LEFT JOIN com_zeapps_crm_products p ON p.id_cat = ca.id
+                  LEFT JOIN com_zeapps_crm_invoice_lines l ON l.id_product = p.id
+                  LEFT JOIN com_zeapps_crm_invoices i ON i.id = l.id_invoice
+                  WHERE i.finalized = '1'
+                        AND l.type = 'product'
+                        AND i.deleted_at IS NULL
+                        AND l.deleted_at IS NULL
+                        AND YEAR(i.date_limit) in (" . ($year - 1) . "," . $year . ")";
+
+        if (isset($where['id_origin'])) {
+            $query .= " AND i.id_origin = " . $where['id_origin'];
+        }
+        if (isset($where['id_cat'])) {
+            $query .= " AND ca.id IN (" . implode(',', $where['id_cat']) . ")";
+        }
+        if (isset($where['country_id'])) {
+            $query .= " AND i.country_id IN (" . implode(',', $where['country_id']) . ")";
+        }
+
+        $query .= " GROUP BY YEAR(i.date_limit), i.id_origin, ca.id";
+
+        return Capsule::select(Capsule::raw($query));
     }
 }
--- Models/Product/Products.php
+++ Models/Product/Products.php
@@ -2,20 +2,21 @@
 
 namespace App\com_zeapps_crm\Models\Product;
 
-use Illuminate\Database\Eloquent\Model ;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 use Zeapps\Core\ModelHelper;
 
-class Products extends Model {
+class Products extends Model
+{
     use SoftDeletes;
 
     static protected $_table = 'com_zeapps_crm_products';
-    protected $table ;
+    protected $table;
 
-    protected $fieldModelInfo ;
+    protected $fieldModelInfo;
 
 
     public function __construct(array $attributes = [])
@@ -51,28 +52,80 @@ class Products extends Model {
         parent::__construct($attributes);
     }
 
-    public static function getSchema() {
-        return $schema = Capsule::schema()->getColumnListing(self::$_table) ;
+    public static function getSchema()
+    {
+        return $schema = Capsule::schema()->getColumnListing(self::$_table);
     }
 
-    public function save(array $options = []) {
+    public function save(array $options = [])
+    {
 
         /******** clean data **********/
-        $this->fieldModelInfo->cleanData($this) ;
+        $this->fieldModelInfo->cleanData($this);
 
 
         /**** to delete unwanted field ****/
-        $this->fieldModelInfo->removeFieldUnwanted($this) ;
+        $this->fieldModelInfo->removeFieldUnwanted($this);
 
         return parent::save($options);
     }
 
-    public static function archive_products($id_arr = NULL){
-        if($id_arr){
-            foreach($id_arr as $id){
-                self::where("id_cat", $id)->update(array('id_cat' => -1)) ;
+    public static function archive_products($id_arr = NULL)
+    {
+        if ($id_arr) {
+            foreach ($id_arr as $id) {
+                self::where("id_cat", $id)->update(array('id_cat' => -1));
             }
         }
         return;
+    }
+
+    public static function top10($year = null, $where = array())
+    {
+        $query = "SELECT SUM(l.total_ht) as total_ht,
+                         p.name as name
+                  FROM com_zeapps_crm_product_categories ca
+                  LEFT JOIN com_zeapps_crm_products p ON p.id_cat = ca.id
+                  LEFT JOIN com_zeapps_crm_invoice_lines l ON l.id_product = p.id
+                  LEFT JOIN com_zeapps_crm_invoices i ON i.id = l.id_invoice
+                  WHERE i.finalized = '1'
+                        AND l.type = 'product'
+                        AND i.deleted_at IS NULL
+                        AND l.deleted_at IS NULL
+                        AND YEAR(i.date_limit) = " . $year;
+
+
+        if (isset($where['id_cat'])) {
+            $query .= " AND ca.id IN (" . implode(',', $where['id_cat']) . ")";
+        }
+
+
+
+        if (isset($where['id_price_list'])) {
+            $query .= " AND i.id_price_list = " . $where['id_price_list'];
+        }
+
+        if (isset($where['id_origin'])) {
+            $query .= " AND i.id_origin = " . $where['id_origin'];
+        }
+
+        if (isset($where['delivery_country_id IN'])) {
+            $query .= " AND i.delivery_country_id IN (" . $where['delivery_country_id IN'] . ")";
+        }
+
+        if (isset($where['delivery_country_id'])) {
+            $query .= " AND i.delivery_country_id = " . $where['delivery_country_id'] ;
+        }
+
+
+
+
+
+
+
+
+        $query .= " GROUP BY p.id ORDER BY total_ht DESC LIMIT 10";
+
+        return Capsule::select(Capsule::raw($query));
     }
 }
