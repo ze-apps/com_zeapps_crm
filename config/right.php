2c58c748e5041ec27f2757b874d8d20c18e9380a
Nicolas Ramel <nicolas.ramel@preview-communication.fr>
2020-08-18 18:15:28 +0200

    droit de base pour la crm

---
+++ config/right.php
@@ -0,0 +1,13 @@
+<?php
+
+$tabRight = array () ;
+$tabRight["id"] = "com_zeapps_crm_read" ;
+$tabRight["module"] = "com_zeapps_crm" ;
+$tabRight["label"] = "Lecture" ;
+$rightZeapps[] = $tabRight ;
+
+$tabRight = array () ;
+$tabRight["id"] = "com_zeapps_crm_write" ;
+$tabRight["module"] = "com_zeapps_crm" ;
+$tabRight["label"] = "Ecriture" ;
+$rightZeapps[] = $tabRight ;
